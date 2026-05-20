eadc99bc1a223cd46f8236b57ecc246d7df08998
Alekhya Bandu <bandualekhya11@gmail.com>
2026-05-20 21:44:02 +0530

    Delete sample.spec.ts

--- sample.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('sample test', async ({ page }) => {
-  await page.goto('https://www.google.com');
-  await expect(page).toHaveTitle(/Google/);
-});
